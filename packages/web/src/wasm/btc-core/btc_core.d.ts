22e5e84d71c60fc3a8be95f70668fc43d5487361
Aditaya08 <aditayash625@gmail.com>
2026-06-23 17:49:22 +0530

    build(wasm): commit compiled wasm bindings to avoid toolchain requirement in build environment

---
+++ packages/web/src/wasm/btc-core/btc_core.d.ts
@@ -0,0 +1,50 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function wasm_analyze_taproot(witness_hex: any): any;
+
+export function wasm_classify_script(script_hex: string): any;
+
+export function wasm_debug_script(script_sig: string, script_pubkey: string, witness_hex: any, raw_tx_hex: string | null | undefined, input_index: number | null | undefined, prevout_value: bigint | null | undefined, prevouts: any): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly wasm_analyze_taproot: (a: any) => [number, number, number];
+    readonly wasm_classify_script: (a: number, b: number) => [number, number, number];
+    readonly wasm_debug_script: (a: number, b: number, c: number, d: number, e: any, f: number, g: number, h: number, i: number, j: bigint, k: any) => [number, number, number];
+    readonly rustsecp256k1_v0_10_0_context_create: (a: number) => number;
+    readonly rustsecp256k1_v0_10_0_context_destroy: (a: number) => void;
+    readonly rustsecp256k1_v0_10_0_default_error_callback_fn: (a: number, b: number) => void;
+    readonly rustsecp256k1_v0_10_0_default_illegal_callback_fn: (a: number, b: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
